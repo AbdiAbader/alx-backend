4f553d3e7e67cb26ec077ccfb3d35782f99bccde
AbdiAbader <abdiprocode@gmail.com>
2023-04-15 11:21:25 +0300

    fix pep8

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,9 +2,10 @@
 """Simple helper"""
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """a tuple of size two containing a start index
     and an end index corresponding to
     the range of indexes"""
     end = page_size * page
-    return(end-page_size, end)
+    return (end-page_size, end)
