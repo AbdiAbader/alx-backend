8af31cbc4e87744817e55f9fbe30d7ada812091c
AbdiAbader <abdiprocode@gmail.com>
2023-04-24 20:05:38 +0300

    first commit

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/python3
+""" basic cache """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    """basic cache class"""
+    def put(self, key, item):
+        """put method"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
---
+++ 0x01-caching/README.md
@@ -0,0 +1,7 @@
+# Caching
+## Caching
+### 0-basic_cache.py
+### 1-fifo_cache.py
+### 2-lifo_cache.py
+### 3-lru_cache.py
+### 4-mru_cache.py
