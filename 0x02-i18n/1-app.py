0f27a5ea4f463116bf0993ef6465eee59a047659
AbdiAbader <abdiprocode@gmail.com>
2023-04-25 16:52:02 +0300

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -11,10 +11,13 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale():
-    """get locale"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+class Config(object):
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 
 @app.route('/', strict_slashes=False)
 def hello():
